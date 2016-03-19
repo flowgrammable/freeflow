a9f90336e8ba56f48e73efb808fcc5a6defa73d0
Michael Gruesen <mgruesen7@gmail.com>
2016-03-19 15:23:10 -0400

    Added wire-sta and wire-tpp scripts, example pcap file

---
+++ scripts/wire/wire_tpp.sh
@@ -0,0 +1,37 @@
+# Set input file.
+input=$PWD/input
+
+# Move to build folder.
+cd ../../build
+if [ $? -ne 0 ];
+	then
+	echo "Error. No build folder found"
+	exit 1
+fi
+
+# Navigate to the fp-lite folder.
+#
+# If there is no release folder, it shouldn't matter.
+cd release
+cd fp-lite
+
+# Start the wire application.
+output=$PWD/wire_tpp.txt
+drivers/wire/fp-wire-epoll-tpp &> $output &
+echo "Wire-TPP started... writing to $output"
+
+# Pause for a second.
+sleep 1
+
+cd ../flowcap
+
+# Start the sink (netcat)
+netcat localhost 5000 &>> /dev/null &
+echo "Sink (netcat) started..."
+
+# Pause for a second.
+sleep 1
+
+# Start the source (flowcap)
+echo "Starting $PWD/flowcap..."
+./flowcap forward $input/smallFlows.pcap 127.0.0.1 5000 100
