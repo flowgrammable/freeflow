9c6cec49ee3cc570412f71c1377c90a17a0a53da
Michael Gruesen <mgruesen7@gmail.com>
2016-05-26 12:51:12 -0400

    Update to use select, flowcap apps

--- scripts/wire/wire_tpp.sh
+++ scripts/wire/wire_tpp.sh
@@ -31,31 +31,30 @@ fi
 WIRE_PID=$!
 echo "Wire-TPP started... writing to $output"
 
-# Pause for a second.
-sleep 2 
 
 cd ../flowcap
 
 # Start the sink (netcat)
-netcat -q 15 -d localhost 5000 &>> /dev/null &
-NC_PID=$!
-echo "Sink (netcat) started..."
+#netcat -q 15 -d localhost 5000 &>> /dev/null &
+#NC_PID=$!
+#echo "Sink (netcat) started..."
+$bin/wire_sink.sh "fetch" 127.0.0.1 &
 
-# Pause for a second.
 sleep 1
-
 # Start the source (flowcap)
 #echo "Starting $PWD/flowcap..."
 #./flowcap forward $input/smallFlows.pcap 127.0.0.1 5000 200 
-time $bin/wire_source.sh 127.0.0.1
-echo "$PWD/flowcap finished. Waiting 2 seconds..."
-sleep 2 
+$bin/wire_source.sh 127.0.0.1 &
+
+#echo "$PWD/flowcap finished. Waiting 2 seconds..."
+#sleep 2 
 
 # Close the sink.
-echo "Closing sink (netcat)..."
-kill -9 $NC_PID
-sleep 1
-echo "Stopping Wire-STA..."
-kill -2 $WIRE_PID
-
+#echo "Closing sink (netcat)..."
+#kill -9 $NC_PID
+#sleep 1
+#echo "Stopping Wire-STA..."
+#kill -2 $WIRE_PID
+wait
+grep -i 'processed' $output
 #more $output
