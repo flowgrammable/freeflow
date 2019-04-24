447b2da6da6cb71f6cc1a96890e3a3f34a939d47
Luke McHale <luke.mchale@tamu.edu>
2019-04-23 20:00:32 -0500

    Adding otool command to notes for future reference.

--- notes/fix-mathematica-wstp.bash
+++ notes/fix-mathematica-wstp.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+otool -l /Applications/Mathematica.app/Contents/SystemFiles/Links/WSTP/DeveloperKit/MacOSX-x86-64/CompilerAdditions/wstp.framework/Versions/4.36/wstp
+
 install_name_tool -id '/Applications/Mathematica.app/Contents/SystemFiles/Links/WSTP/DeveloperKit/MacOSX-x86-64/CompilerAdditions/wstp.framework/Versions/4.36/wstp' /Applications/Mathematica.app/Contents/SystemFiles/Links/WSTP/DeveloperKit/MacOSX-x86-64/CompilerAdditions/wstp.framework/Versions/4.36/wstp
